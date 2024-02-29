7170f3c48ccbfeb8cb820e561e7a6551abd34168
GeneralMing <advaithkrishna7@gmail.com>
2024-02-28 22:34:05 -0500

    added auto db connect

--- prov.tf
+++ prov.tf
@@ -1,11 +1,12 @@
-
-
-
 terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "5.16.0"
+      version = ">=4.0, <5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
     }
   }
 }
